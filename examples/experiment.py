13d5f67f0d6c56501a24d5feca404402e4a920a2
Jonas Kopka <jonas.kopka@student.hpi.uni-potsdam.de>
2019-06-27 17:52:40 +0200

    Refactoring databast/fact condition and adding Execution time measurement.

--- examples/experiment.py
+++ examples/experiment.py
@@ -1,5 +1,5 @@
 from fiber.cohort import Cohort
-from fiber.condition import Procedure, Diagnosis
+from fiber.condition import Procedure, Diagnosis, Patient
 from fiber.database.table import fact
 min_age = fact.AGE_IN_DAYS > 365 * 18
 
@@ -7,7 +7,15 @@ hs = (
     Procedure(code='35.%').with_(min_age) |
     Procedure(code='36.1%').with_(min_age)
 )
+ps = (
+    Patient(gender='Male') &
+    Patient(religion='Catholic')
+)
 aki = Diagnosis(code='584.9', context='ICD-9')
 heart_surgery_cohort = Cohort(hs)
+patient_cohort = Cohort(ps)
+
 print(len(heart_surgery_cohort))
 print(len(heart_surgery_cohort.exclude(['2241492414', '2117141414']).mrns()))
+
+print(len(patient_cohort))
